130b880f5c4336d7652073986942771b7237a23c
root <root@localhost.localdomain>
2017-12-21 12:10:59 -0400

    tabla eddy2

--- base/funciones/patch000001.sql
+++ base/funciones/patch000001.sql
@@ -1,4 +1,8 @@
-/*****************************I-SCP-EGS-SHOP-0-21/12/2017*************/
-alter table segu.tprocedimiento
-alter column id_funcion set not null;
-/*****************************F-SCP-RCM-SHOP-0-21/12/2017*************/
+/*****************************I-SCP-EGS-SHOP-1-21/12/2017*************/
+CREATE TABLE shop.tproveedor (
+  id_proveedor SERIAL,
+  nombre VARCHAR(255),
+  CONSTRAINT pk_tproveedor_id_proveedor PRIMARY KEY(id_proveedor)
+) INHERITS (pxp.tbase)
+WITH (oids = false);
+/*****************************F-SCP-EGS-SHOP-1-21/12/2017*************/
